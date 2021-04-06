aa48981ac1b9385c0f56e8ce8f2e8350c891f5b0
nelsonombuya <68895654+neodevelopment@users.noreply.github.com>
2021-04-06 11:37:42 +0300

    Refactor: Loading Page > Removed a few lines of code > Removed test waiting time on loading screen

--- lib/logic/bloc/loading_bloc.dart
+++ lib/logic/bloc/loading_bloc.dart
@@ -12,14 +12,9 @@ class LoadingBloc extends Bloc<LoadingEvent, LoadingState> {
   @override
   Stream<LoadingState> mapEventToState(LoadingEvent event) async* {
     if (event is LoadingStarted) {
-      // TODO Add Loading Tasks
-      await Future.delayed(Duration(seconds: 8));
-      bool loading = false;
-      if (loading) {
-        yield LoadingComplete(firstPage: '/dashboard');
-      } else {
-        yield LoadingComplete(firstPage: '/welcome');
-      }
+      bool _loading = false; // TODO Add Loading Events
+      String _firstPage = _loading ? '/dashboard' : '/welcome';
+      yield LoadingComplete(firstPage: _firstPage);
     }
   }
 }
