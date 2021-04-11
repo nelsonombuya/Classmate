44ad56eed216497ba8700036b310086e9e932234
nelsonombuya <68895654+neodevelopment@users.noreply.github.com>
2021-04-11 13:06:08 +0300

    Refactor: Remove Relative Scale from Sign Up Page

--- lib/presentation/pages/sign_up/sign_up_page.dart
+++ lib/presentation/pages/sign_up/sign_up_page.dart
@@ -4,7 +4,7 @@ import 'package:classmate/presentation/widgets/custom_textFormField_widget.dart'
 import 'package:classmate/presentation/widgets/custom_form_view_widget.dart';
 import 'package:classmate/presentation/widgets/custom_header_widget.dart';
 import 'package:classmate/constants/validators.dart';
-import 'package:relative_scale/relative_scale.dart';
+import 'package:classmate/constants/device.dart';
 import 'package:flutter/Material.dart';
 
 class SignUpPage extends StatefulWidget {
@@ -45,138 +45,130 @@ class _SignUpPageState extends State<SignUpPage> {
 
   @override
   Widget build(BuildContext context) {
+    Device().init(context);
     return FormView(
-      child: RelativeBuilder(
-        builder: (context, height, width, sy, sx) {
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+        child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // # Sign Up Header Text
+        CustomHeader(
+          heading: 'SIGN UP',
+          subheading: 'Create a new account',
+        ),
+
+        // # Sized Box for spacing
+        SizedBox(height: Device.height(2.0)),
+
+        // # Form
+        Form(
+          key: _formKey,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          child: Column(
             children: [
-              // # Sign Up Header Text
-              CustomHeader(
-                heading: 'SIGN UP',
-                subheading: 'Create a new account',
+              // # Names
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // First Name
+                  Expanded(
+                    child: CustomTextFormField(
+                      label: 'First Name',
+                      enabled: _areThingsEnabled,
+                      keyboardType: TextInputType.name,
+                      validator: Validator.nameValidator,
+                    ),
+                  ),
+
+                  // Spacing Between Fields
+                  SizedBox(width: Device.width(4.0)),
+
+                  // Last Name
+                  Expanded(
+                    child: CustomTextFormField(
+                      label: 'Last Name',
+                      enabled: _areThingsEnabled,
+                      keyboardType: TextInputType.name,
+                      validator: Validator.nameValidator,
+                    ),
+                  ),
+                ],
               ),
 
               // # Sized Box for spacing
-              SizedBox(height: sy(15.0)),
+              SizedBox(height: Device.height(3.0)),
 
-              // # Form
-              Form(
-                key: _formKey,
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                child: Column(
-                  children: [
-                    // # Names
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        // First Name
-                        Expanded(
-                          child: CustomTextFormField(
-                            label: 'First Name',
-                            enabled: _areThingsEnabled,
-                            keyboardType: TextInputType.name,
-                            validator: Validator.nameValidator,
-                          ),
-                        ),
+              // # Email
+              CustomTextFormField(
+                label: 'Email Address',
+                enabled: _areThingsEnabled,
+                validator: Validator.emailValidator,
+                keyboardType: TextInputType.emailAddress,
+              ),
 
-                        // Spacing Between Fields
-                        SizedBox(width: sx(20.0)),
+              // # Sized Box for spacing
+              SizedBox(height: Device.height(3.0)),
 
-                        // Last Name
-                        Expanded(
-                          child: CustomTextFormField(
-                            label: 'Last Name',
-                            enabled: _areThingsEnabled,
-                            keyboardType: TextInputType.name,
-                            validator: Validator.nameValidator,
-                          ),
-                        ),
-                      ],
-                    ),
+              // # Password
+              CustomTextFormField(
+                label: 'Password',
+                enabled: _areThingsEnabled,
+                onChanged: (value) =>
+                    setState(() => _temporaryPasswordHolder = value),
+                obscureText: !_showPassword,
+                validator: Validator.passwordValidator,
+                keyboardType: TextInputType.visiblePassword,
+                suffixIcon: _showPassword
+                    ? Icons.visibility_outlined
+                    : Icons.visibility_off_outlined,
+                suffixIconAction: () =>
+                    setState(() => _showPassword = !_showPassword),
+              ),
 
-                    // # Sized Box for spacing
-                    SizedBox(height: sy(15.0)),
+              // # Sized Box for spacing
+              SizedBox(height: Device.height(3.0)),
 
-                    // # Email
-                    CustomTextFormField(
-                      label: 'Email Address',
-                      enabled: _areThingsEnabled,
-                      validator: Validator.emailValidator,
-                      keyboardType: TextInputType.emailAddress,
-                    ),
+              // # Password Confirmation
+              CustomTextFormField(
+                label: 'Confirm Password',
+                enabled: _areThingsEnabled,
+                obscureText: !_showPassword,
+                validator: confirmPasswordValidator,
+                keyboardType: TextInputType.visiblePassword,
+                suffixIcon: _showPassword
+                    ? Icons.visibility_outlined
+                    : Icons.visibility_off_outlined,
+                suffixIconAction: () =>
+                    setState(() => _showPassword = !_showPassword),
+              ),
 
-                    // # Sized Box for spacing
-                    SizedBox(height: sy(15.0)),
+              // # Sized Box for spacing
+              SizedBox(height: Device.height(8.0)),
 
-                    // # Password
-                    CustomTextFormField(
-                      label: 'Password',
-                      enabled: _areThingsEnabled,
-                      onChanged: (value) =>
-                          setState(() => _temporaryPasswordHolder = value),
-                      obscureText: !_showPassword,
-                      validator: Validator.passwordValidator,
-                      keyboardType: TextInputType.visiblePassword,
-                      suffixIcon: _showPassword
-                          ? Icons.visibility_outlined
-                          : Icons.visibility_off_outlined,
-                      suffixIconAction: () =>
-                          setState(() => _showPassword = !_showPassword),
-                    ),
-
-                    // # Sized Box for spacing
-                    SizedBox(height: sy(15.0)),
-
-                    // # Password Confirmation
-                    CustomTextFormField(
-                      label: 'Confirm Password',
-                      enabled: _areThingsEnabled,
-                      obscureText: !_showPassword,
-                      validator: confirmPasswordValidator,
-                      keyboardType: TextInputType.visiblePassword,
-                      suffixIcon: _showPassword
-                          ? Icons.visibility_outlined
-                          : Icons.visibility_off_outlined,
-                      suffixIconAction: () =>
-                          setState(() => _showPassword = !_showPassword),
-                    ),
-
-                    // # Sized Box for spacing
-                    SizedBox(height: sy(40.0)),
-
-                    // # Sign Up Button
-                    Center(
-                      child: CustomLoadingElevatedButton(
-                        child: Text(
-                          'Sign Up',
-                          style: Theme.of(context)
-                              .textTheme
-                              .button
-                              .copyWith(fontSize: sy(9)),
-                        ),
-                        onPressed: () {
-                          if (_formKey.currentState.validate()) {
-                            setState(() {
-                              _showPassword = false;
-                              _areThingsEnabled = false;
-                            });
-                            return widget.signUp();
-                          }
-                          return false;
-                        },
-                        onSuccess: () {},
-                        onFailure: () =>
-                            setState(() => _areThingsEnabled = true),
-                      ),
-                    ),
-                  ],
+              // # Sign Up Button
+              Center(
+                child: CustomLoadingElevatedButton(
+                  child: Text(
+                    'Sign Up',
+                    style: Theme.of(context).textTheme.button,
+                  ),
+                  onPressed: () {
+                    if (_formKey.currentState.validate()) {
+                      setState(() {
+                        _showPassword = false;
+                        _areThingsEnabled = false;
+                      });
+                      return widget.signUp();
+                    }
+                    return false;
+                  },
+                  onSuccess: () {},
+                  onFailure: () => setState(() => _areThingsEnabled = true),
                 ),
-              )
+              ),
             ],
-          );
-        },
-      ),
-    );
+          ),
+        )
+      ],
+    ));
   }
 }
