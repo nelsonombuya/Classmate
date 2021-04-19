0c24aeec5640db754d2279cc69c0d719f56ff263
nelsonombuya <68895654+neodevelopment@users.noreply.github.com>
2021-04-20 00:29:07 +0300

    Refactor: Moving Things Around > Moved Firebase Initialization to App File > Cleaned up init file

--- lib/app.dart
+++ lib/app.dart
@@ -1,25 +1,60 @@
 // # Imports
 import 'package:classmate/bloc/auth/auth_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:classmate/constants/routes.dart';
 import 'package:classmate/constants/themes.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:sailor/sailor.dart';
 
+import 'presentation/pages/splash/splash_page.dart';
+
 /// # Flutter App Settings
 /// * Best place to add global BLoC Providers
+/// * Also where the firebase app is initialized
 class ClassMate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => AuthBloc()..add(AuthStarted()),
-      child: MaterialApp(
-        navigatorObservers: [SailorLoggingObserver()], // * For Navigation Logs
-        onGenerateRoute: Routes.sailor.generator(), // * For Routes (Sailor)
-        navigatorKey: Routes.sailor.navigatorKey, // * For Routes (Sailor)
-        darkTheme: Themes.darkTheme,
-        theme: Themes.lightTheme,
-        title: 'ClassMate',
+      child: FutureBuilder(
+        future: Firebase.initializeApp(),
+        builder: (context, snapshot) {
+          // * Running the main app after firebase app has initialized
+          if (snapshot.connectionState == ConnectionState.done)
+            return MaterialApp(
+              // # Sailor Navigation
+              // These are used by sailor to ease navigation
+              navigatorObservers: [SailorLoggingObserver()],
+              onGenerateRoute: Routes.sailor.generator(),
+              navigatorKey: Routes.sailor.navigatorKey,
+
+              // # Themes
+              darkTheme: Themes.darkTheme,
+              theme: Themes.lightTheme,
+
+              title: 'ClassMate',
+            );
+
+          // ! In case of an error
+          if (snapshot.hasError)
+            return MaterialApp(
+              // # Themes
+              darkTheme: Themes.darkTheme,
+              theme: Themes.lightTheme,
+              title: 'ClassMate',
+              home: SplashPage(error: snapshot.error),
+            );
+
+          // * Splash Page before the firebase app initializes
+          return MaterialApp(
+            // # Themes
+            darkTheme: Themes.darkTheme,
+            theme: Themes.lightTheme,
+            title: 'ClassMate',
+            home: SplashPage(hold: true),
+          );
+        },
       ),
     );
   }
