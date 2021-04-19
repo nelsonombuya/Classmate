439d0b0a351787d55a696b5c1a3dabd4a26ef2ed
nelsonombuya <68895654+neodevelopment@users.noreply.github.com>
2021-04-19 09:40:04 +0300

    Chore: Minor Clean Up > Fixing issues with Splash

---
+++ lib/presentation/widgets/asciimoji_widget.dart
@@ -0,0 +1,45 @@
+// # Imports
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+
+/// # ASCIImoji
+/// Displays an animated list of ASCIImoji
+/// Has 2 Animation Styles
+///   Typed [Default]
+///   Console
+class ASCIImoji extends StatelessWidget {
+  // # Parameters
+  final textStyle;
+
+  // # Constructor
+  ASCIImoji(this.textStyle);
+
+  AnimatedText typeWriterText({@required String text}) {
+    return TyperAnimatedText(text,
+        textAlign: TextAlign.center,
+        textStyle: textStyle,
+        speed: Duration(milliseconds: 250));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedTextKit(
+      animatedTexts: [
+        typeWriterText(text: ':-)'),
+        typeWriterText(text: ':-D'),
+        typeWriterText(text: ':-O'),
+        typeWriterText(text: 'ʕ·͡ᴥ·ʔ'),
+        typeWriterText(text: '•͡˘㇁•͡˘'),
+        typeWriterText(text: '(◕ᴥ◕ʋ)'),
+        typeWriterText(text: '(̿▀̿ ̿Ĺ̯̿̿▀̿ ̿)̄'),
+        typeWriterText(text: 'ʕっ•ᴥ•ʔっ'),
+        typeWriterText(text: '(͡ ° ͜ʖ ͡ °)'),
+        typeWriterText(text: '(｡◕‿‿◕｡)'),
+        typeWriterText(text: '( 0 _ 0 )'),
+        typeWriterText(text: '(˵ ͡° ͜ʖ ͡°˵)'),
+        typeWriterText(text: '¯\\(°_o)/¯'),
+      ],
+      repeatForever: true,
+    );
+  }
+}
