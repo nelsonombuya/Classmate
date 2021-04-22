bb81da658691185924a5bbcab2dc373f10c19d1f
nelsonombuya <68895654+neodevelopment@users.noreply.github.com>
2021-04-22 22:19:27 +0300

    Add: Timeline picker on Dashboard Page > Set different themes for dark and light theme

--- lib/presentation/pages/dashboard/dashboard_page.dart
+++ lib/presentation/pages/dashboard/dashboard_page.dart
@@ -2,6 +2,7 @@
 import 'package:classmate/bloc/auth/auth_bloc.dart';
 import 'package:classmate/constants/device.dart';
 import 'package:classmate/data/models/user_model.dart';
+import 'package:classmate/presentation/pages/dashboard/date_picker_carousel.dart';
 import 'package:flutter/material.dart';
 
 /// # Dashboard Page
@@ -15,14 +16,31 @@ class DashboardPage extends StatelessWidget {
       DashboardView(user: user, auth: authBloc);
 }
 
-class DashboardView extends StatelessWidget {
+class DashboardView extends StatefulWidget {
   DashboardView({this.user, this.auth});
   final AuthBloc auth;
   final user;
 
   @override
+  _DashboardViewState createState() => _DashboardViewState();
+}
+
+class _DashboardViewState extends State<DashboardView> {
+  DateTime _selectedDate = DateTime.now();
+
+  @override
   Widget build(BuildContext context) {
     Device().init(context);
-    return Container(color: Colors.redAccent);
+    void _onDateChanged(DateTime date) => setState(() => _selectedDate = date);
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        DatePickerCarousel(
+          onDateChange: _onDateChanged,
+          selectedDate: _selectedDate,
+        ),
+      ],
+    );
   }
 }
---
+++ lib/presentation/pages/dashboard/date_picker_carousel.dart
@@ -0,0 +1,69 @@
+import 'package:date_picker_timeline/date_picker_timeline.dart';
+import 'package:flutter/material.dart';
+
+import '../../../constants/device.dart';
+
+class DatePickerCarousel extends StatelessWidget {
+  DatePickerCarousel({
+    Key key,
+    @required this.onDateChange,
+    @required this.selectedDate,
+  }) : super(key: key);
+
+  DateTime _getDate(DateTime d) => DateTime(d.year, d.month, d.day);
+
+  final DateTime _now = DateTime.now();
+  final Function onDateChange;
+  final DateTime selectedDate;
+
+  @override
+  Widget build(BuildContext context) {
+    Device().init(context);
+
+    DateTime _beginningOfTheWeek =
+        _getDate(_now.subtract(Duration(days: _now.weekday - 1)));
+
+    // Setting text and button colors
+    Color _dateTextStyleColor, _dayTextStyleColor, _selectedDateColor;
+
+    if (Device.brightness == Brightness.light) {
+      _dateTextStyleColor = Colors.black87;
+      _selectedDateColor = Colors.black87;
+      _dayTextStyleColor = Colors.grey;
+    } else {
+      _dateTextStyleColor = Colors.white70;
+      _selectedDateColor = Colors.white38;
+      _dayTextStyleColor = Colors.white54;
+    }
+
+    // Setting Text Styles
+    TextStyle _dateTextStyle = TextStyle(
+      fontWeight: FontWeight.w200,
+      color: _dateTextStyleColor,
+      fontFamily: "Averta",
+      fontSize: 24,
+    );
+
+    TextStyle _dayTextStyle = _dateTextStyle.copyWith(
+      fontSize: 11,
+      color: _dayTextStyleColor,
+    );
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        DatePicker(
+          _beginningOfTheWeek,
+          daysCount: 7,
+          onDateChange: onDateChange,
+          dayTextStyle: _dayTextStyle,
+          dateTextStyle: _dateTextStyle,
+          monthTextStyle: _dayTextStyle,
+          selectedTextColor: Colors.white,
+          selectionColor: _selectedDateColor,
+          initialSelectedDate: DateTime.now(),
+        ),
+      ],
+    );
+  }
+}
