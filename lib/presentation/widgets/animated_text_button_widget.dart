dfdb2b5c13207b85c32df0e91787cdbb5cfe26e4
nelsonombuya <68895654+neodevelopment@users.noreply.github.com>
2021-04-26 23:55:00 +0300

    Refactor: Animted Text Button > Changed the way it handles error and success states > Now needs an Exception to be thrown to show an error state > Turned off success state, now closes the modal instead

--- lib/presentation/widgets/animated_text_button_widget.dart
+++ lib/presentation/widgets/animated_text_button_widget.dart
@@ -1,35 +1,29 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:future_button/future_button.dart';
 
 import '../../constants/device.dart';
 
 class AnimatedTextButton extends StatelessWidget {
-  const AnimatedTextButton({Key key, this.onPressed, this.label})
-      : super(key: key);
-
   final String label;
-
-  // * Best be a Future<void> function
-  // * Throw an exception in the function
-  // * if you want the button to show an error state
   final Function onPressed;
+
+  const AnimatedTextButton({Key key, this.onPressed, this.label});
 
   @override
   Widget build(BuildContext context) {
-    // ### Used for scaling the button properly on different screen sizes
     Device().init(context);
 
-    return FutureFlatButton(
-      showResult: true,
+    return FutureCupertinoButton(
       onPressed: onPressed,
+      progressIndicatorLocation: ProgressIndicatorLocation.right,
       child: Text(
         label,
-        style: TextStyle(
-          fontSize: Device.height(2.5),
-          color: CupertinoColors.activeBlue,
-        ),
+        style: Theme.of(context).textTheme.button.copyWith(
+              fontSize: Device.height(2.5),
+              color: CupertinoColors.activeBlue,
+            ),
       ),
-      progressIndicatorLocation: ProgressIndicatorLocation.right,
     );
   }
 }
