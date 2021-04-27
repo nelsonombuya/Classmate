641e82e94bd9a0b2e658df9910fba5bb3af5d97e
nelsonombuya <68895654+neodevelopment@users.noreply.github.com>
2021-04-28 01:01:05 +0300

    Refactor: Event Model

--- lib/data/models/event_model.dart
+++ lib/data/models/event_model.dart
@@ -2,14 +2,14 @@ class EventModel {
   final String docId;
   final String title;
   final String description;
-  final DateTime startingTime;
-  final DateTime endingTime;
+  final DateTime startingDate;
+  final DateTime endingDate;
 
   const EventModel({
     this.docId,
     this.title,
     this.description,
-    this.startingTime,
-    this.endingTime,
+    this.startingDate,
+    this.endingDate,
   });
 }
