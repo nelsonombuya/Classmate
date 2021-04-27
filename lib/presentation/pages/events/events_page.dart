fb0133b694513e1d5c27b4a28a568040be36733e
nelsonombuya <68895654+neodevelopment@users.noreply.github.com>
2021-04-28 01:01:35 +0300

    Add: Events Page > Initial Design

--- lib/presentation/pages/events/events_page.dart
+++ lib/presentation/pages/events/events_page.dart
@@ -1,12 +1,56 @@
-// # Imports
-import 'package:classmate/constants/device.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-/// # Events Page
-class EventsPage extends StatelessWidget {
+import '../../../constants/device.dart';
+
+class EventsPage extends StatefulWidget {
+  @override
+  _EventsPageState createState() => _EventsPageState();
+}
+
+class _EventsPageState extends State<EventsPage> {
+  DateTime _selectedDay, _focusedDay = DateTime.now();
+
+  DateTime _firstDay = DateTime(DateTime.now().year - 20);
+  DateTime _lastDay = DateTime(DateTime.now().year + 20);
+
+  CalendarFormat _calendarFormat = CalendarFormat.week;
+
   @override
   Widget build(BuildContext context) {
     Device().init(context);
-    return Text('Events Page Bruv');
+
+    return ListView(
+      children: <Widget>[
+        Container(
+          color: Device.brightness == Brightness.light
+              ? CupertinoColors.systemGroupedBackground
+              : CupertinoColors.darkBackgroundGray,
+          child: TableCalendar(
+            lastDay: _lastDay,
+            firstDay: _firstDay,
+            focusedDay: _focusedDay,
+            calendarFormat: _calendarFormat,
+            selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+            onDaySelected: (selectedDay, focusedDay) {
+              setState(
+                () {
+                  _selectedDay = selectedDay;
+                  _focusedDay = focusedDay;
+                },
+              );
+            },
+            onFormatChanged: (format) {
+              setState(
+                () {
+                  _calendarFormat = format;
+                },
+              );
+            },
+          ),
+        ),
+      ],
+    );
   }
 }
