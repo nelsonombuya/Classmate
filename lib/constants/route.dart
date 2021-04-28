90ecbc19893df5e5bf7809d1fb9c8436be00230b
nelsonombuya <68895654+neodevelopment@users.noreply.github.com>
2021-04-28 22:39:57 +0300

    Update: Changing Routes > Moved App's contents to Main for centralization > Updated routes to use a modified flutter's default routing

---
+++ lib/constants/route.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import '../presentation/pages/home/home.dart';
+import '../presentation/pages/sign_in/sign_in_page.dart';
+import '../presentation/pages/sign_up/sign_up_page.dart';
+import '../presentation/pages/splash/splash_page.dart';
+import '../presentation/pages/welcome/welcome_page.dart';
+
+const String welcomePage = 'welcome';
+const String splashPage = 'splash';
+const String signInPage = 'sign_in';
+const String signUpPage = 'sign_up';
+const String homePage = 'home';
+
+Route<dynamic> controller(RouteSettings settings) {
+  switch (settings.name) {
+    case welcomePage:
+      return MaterialPageRoute(builder: (context) => WelcomePage());
+    case splashPage:
+      return MaterialPageRoute(builder: (context) => SplashPage());
+    case homePage:
+      return MaterialPageRoute(builder: (context) => HomePage());
+    case signInPage:
+      return MaterialPageRoute(builder: (context) => SignInPage());
+    case signUpPage:
+      return MaterialPageRoute(builder: (context) => SignUpPage());
+    default:
+      throw ('This route name does not exit');
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,3 @@
-// # Imports
-import 'package:classmate/app.dart';
-import 'package:classmate/bloc/watchtower_observer.dart';
 import 'package:equatable/equatable.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
@@ -8,17 +5,41 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'constants/routes.dart';
+import 'bloc/auth/auth_bloc.dart';
+import 'bloc/notification/notification_bloc.dart';
+import 'bloc/watchtower_observer.dart';
+import 'constants/route.dart' as route;
+import 'constants/themes.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   EquatableConfig.stringify = kDebugMode;
   await Firebase.initializeApp();
   Bloc.observer = Watchtower();
-  Routes.createRoutes();
 
   // TODO Restructure App for Landscape Orientation
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
       .then((value) => runApp(ClassMate()));
+}
+
+class ClassMate extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<AuthBloc>(
+          create: (context) => AuthBloc()..add(AuthStarted()),
+        ),
+        BlocProvider<NotificationBloc>(
+          create: (context) => NotificationBloc(),
+        ),
+      ],
+      child: MaterialApp(
+        onGenerateRoute: route.controller,
+        darkTheme: Themes.darkTheme,
+        theme: Themes.lightTheme,
+        title: 'Classmate',
+      ),
+    );
+  }
 }
