6ab7d9d26791ededfc1ea0e4da9dc99febc79f01
nelsonombuya <68895654+neodevelopment@users.noreply.github.com>
2021-04-29 00:34:36 +0300

    Refactor: Init Page > Added a few comments to explain the widget functionality > Renamed Home to HomePage

--- lib/presentation/init.dart
+++ lib/presentation/init.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../bloc/auth/auth_bloc.dart';
 import '../bloc/notification/notification_bloc.dart';
+import '../constants/device.dart';
 import 'pages/home/home.dart';
 import 'pages/splash/splash_page.dart';
 import 'pages/welcome/welcome_page.dart';
@@ -16,44 +17,50 @@ import 'widgets/dialog_widget.dart';
 /// * Holds the Auth Builder:
 ///   - Sends the user directly to the dashboard if they're authenticated
 ///   - Kicks them back to the Welcome Page if they're not signed in
+///
+/// * Holds the Device Query Inherited Widget
+///   - Used in the scaling of widgets across the app
 class Init extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BlocListener<NotificationBloc, NotificationState>(
-      listener: (context, state) {
-        if (state is ShowSnackBar) {
-          CustomSnackBar(
-            context,
-            message: state.message,
-            type: state.notificationType,
-          );
-        }
-        if (state is ShowDialogBox) {
-          CustomDialog(
-            context,
-            title: state.title,
-            description: state.message,
-            type: state.notificationType,
-            descriptionIcon: state.descriptionIcon,
-            positiveActionIcon: state.positiveActionIcon,
-            positiveActionLabel: state.positiveActionLabel,
-            positiveActionOnPressed: state.positiveActionOnPressed,
-            negativeActionLabel: state.negativeActionLabel,
-            negativeActionOnPressed: state.negativeActionOnPressed,
-          );
-        }
-      },
-      child: BlocBuilder<AuthBloc, AuthState>(
-        builder: (context, state) {
-          if (state is Unauthenticated) return WelcomePage();
-
-          if (state is Authenticated) return Home();
-
-          if (state is AuthenticationError)
-            return SplashPage(error: state.message);
-
-          return SplashPage();
+    return DeviceQuery(
+      context,
+      BlocListener<NotificationBloc, NotificationState>(
+        listener: (context, state) {
+          if (state is ShowSnackBar) {
+            CustomSnackBar(
+              context,
+              message: state.message,
+              type: state.notificationType,
+            );
+          }
+          if (state is ShowDialogBox) {
+            CustomDialog(
+              context,
+              title: state.title,
+              description: state.message,
+              type: state.notificationType,
+              descriptionIcon: state.descriptionIcon,
+              positiveActionIcon: state.positiveActionIcon,
+              positiveActionLabel: state.positiveActionLabel,
+              positiveActionOnPressed: state.positiveActionOnPressed,
+              negativeActionLabel: state.negativeActionLabel,
+              negativeActionOnPressed: state.negativeActionOnPressed,
+            );
+          }
         },
+        child: BlocBuilder<AuthBloc, AuthState>(
+          builder: (context, state) {
+            if (state is Unauthenticated) return WelcomePage();
+
+            if (state is Authenticated) return HomePage();
+
+            if (state is AuthenticationError)
+              return SplashPage(error: state.message);
+
+            return SplashPage();
+          },
+        ),
       ),
     );
   }
