bc147073f995c218bc6a2f58e90580f0ac3f95c4
nelsonombuya <68895654+neodevelopment@users.noreply.github.com>
2021-04-29 11:26:22 +0300

    Refactor: Splash Page > After Removing Animated Text Kit > Reduced number of pages to streamline splash screen view > Simplified animations > Added a message mode for showing messages (eg. Error Messages)

--- lib/presentation/pages/splash/splash_page.dart
+++ lib/presentation/pages/splash/splash_page.dart
@@ -1,26 +1,65 @@
-import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import '../../../constants/device.dart';
+import '../../../constants/device_query.dart';
+import 'splash_text.dart';
 
-class SplashPage extends StatelessWidget {
-  final String _firstString = 'Class';
-  final String _secondString = 'Mate';
-  final Color _secondStringColor = Colors.blue;
+class SplashPage extends StatefulWidget {
+  final String? errorMessage;
+
+  SplashPage({this.errorMessage});
+
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  late final String _logo;
+  late final String _firstString;
+  late final Color _firstStringColor;
+  late final String _secondString;
+  late final Color _secondStringColor;
+  late final double _fontSize;
 
   @override
   Widget build(BuildContext context) {
     final DeviceQuery? _device = DeviceQuery.of(context);
     final TextStyle? _splashTextStyle = Theme.of(context).textTheme.headline2;
 
-    if (_device == null) throw Exception('DeviceQuery should not be null.');
-    if (_splashTextStyle == null)
-      throw Exception('Theme Inherited Widget should not be null.');
+    // TODO Implement Error Handler ❗
+    if (_device == null) {
+      throw Exception('DeviceQuery should not be null. ❗');
+    }
+    // TODO Implement Error Handler ❗
+    if (_splashTextStyle == null) {
+      throw Exception('Theme Inherited Widget should not be null. ❗');
+    }
 
-    final double _fontSize = _device.safeHeight(4.5);
-    final String _logo =
-        _device.brightness == Brightness.light ? 'black_plain' : 'white_plain';
+    if (widget.errorMessage != null) {
+      _logo = 'red_plain';
+      _firstString = 'Something went wrong';
+      _firstStringColor = CupertinoColors.destructiveRed;
+      _secondString = widget.errorMessage!;
+      _secondStringColor = CupertinoColors.destructiveRed;
+    } else {
+      _logo = _device.brightness == Brightness.light
+          ? 'black_plain'
+          : 'white_plain';
+      _firstString = 'Class';
+      _firstStringColor = CupertinoColors.white;
+      _secondString = 'mate';
+      _secondStringColor = Colors.blue;
+    }
+    _fontSize = _device.safeHeight(4.5);
+
+    // TODO Implement Loader Animation ✨
+    double _opacity = 0.0;
+    Future<void> _opacitySwitcher() async {
+      await Future.delayed(Duration(seconds: 1));
+      setState(() => _opacity = 1.0);
+    }
+
+    _opacitySwitcher();
 
     return Scaffold(
       body: Stack(
@@ -40,43 +79,18 @@ class SplashPage extends StatelessWidget {
             left: _device.safeWidth(4.0),
             top: _device.safeHeight(65.0),
             right: _device.safeWidth(4.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                AnimatedTextKit(
-                  isRepeatingAnimation: false,
-                  animatedTexts: [
-                    FadeAnimatedText(
-                      _firstString,
-                      fadeInEnd: 0.2,
-                      fadeOutBegin: 0.8,
-                      textAlign: TextAlign.center,
-                      duration: Duration(seconds: 5),
-                      textStyle: _splashTextStyle.copyWith(
-                        fontWeight: FontWeight.w300,
-                        fontSize: _fontSize,
-                      ),
-                    ),
-                  ],
-                ),
-                AnimatedTextKit(
-                  isRepeatingAnimation: false,
-                  animatedTexts: [
-                    FadeAnimatedText(
-                      _secondString,
-                      fadeInEnd: 0.2,
-                      fadeOutBegin: 0.8,
-                      textAlign: TextAlign.center,
-                      duration: Duration(seconds: 5),
-                      textStyle: _splashTextStyle.copyWith(
-                        fontWeight: FontWeight.w600,
-                        color: _secondStringColor,
-                        fontSize: _fontSize,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
+            child: AnimatedOpacity(
+              opacity: _opacity,
+              duration: Duration(seconds: 3),
+              child: SplashText(
+                messageMode: (widget.errorMessage == null),
+                firstString: _firstString,
+                firstStringColor: _firstStringColor,
+                secondString: _secondString,
+                secondStringColor: _secondStringColor,
+                fontSize: _fontSize,
+                splashTextStyle: _splashTextStyle,
+              ),
             ),
           ),
         ],
---
+++ lib/presentation/pages/splash/splash_text.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class SplashText extends StatelessWidget {
+  const SplashText({
+    Key? key,
+    this.messageMode = false,
+    required this.firstString,
+    required this.splashTextStyle,
+    required this.firstStringColor,
+    required this.fontSize,
+    required this.secondString,
+    required this.secondStringColor,
+  }) : super(key: key);
+
+  final String firstString;
+  final TextStyle splashTextStyle;
+  final Color firstStringColor;
+  final double fontSize;
+  final String secondString;
+  final Color secondStringColor;
+  final bool messageMode;
+
+  @override
+  Widget build(BuildContext context) {
+    return messageMode
+        ? Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                firstString,
+                style: splashTextStyle.copyWith(
+                  fontWeight: FontWeight.w300,
+                  color: firstStringColor,
+                  fontSize: fontSize,
+                ),
+              ),
+              Text(
+                secondString,
+                style: splashTextStyle.copyWith(
+                  fontWeight: FontWeight.w600,
+                  color: secondStringColor,
+                  fontSize: fontSize,
+                ),
+              ),
+            ],
+          )
+        : Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                firstString,
+                style: splashTextStyle.copyWith(
+                  fontWeight: FontWeight.w300,
+                  color: firstStringColor,
+                  fontSize: fontSize,
+                ),
+              ),
+              Text(
+                secondString,
+                style: splashTextStyle.copyWith(
+                  color: secondStringColor,
+                  fontSize: fontSize - 1,
+                ),
+              ),
+            ],
+          );
+  }
+}
