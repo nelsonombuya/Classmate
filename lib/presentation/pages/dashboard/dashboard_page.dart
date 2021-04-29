4e1fdcc1a4d30261a49cf7db9fcfc1b65aacbb15
nelsonombuya <68895654+neodevelopment@users.noreply.github.com>
2021-04-29 18:54:27 +0300

    Refactor: Dashboard Page > Removed Date Picker Timeline (Not Null Safe) > Remove Avatar Widget

--- lib/presentation/pages/dashboard/dashboard_page.dart
+++ lib/presentation/pages/dashboard/dashboard_page.dart
@@ -1,34 +1,13 @@
 import 'package:flutter/material.dart';
 
-import '../../../constants/device.dart';
-import 'date_picker_carousel.dart';
-
 class DashboardPage extends StatefulWidget {
   @override
   _DashboardPageState createState() => _DashboardPageState();
 }
 
 class _DashboardPageState extends State<DashboardPage> {
-  DateTime _selectedDate = DateTime.now();
-
   @override
   Widget build(BuildContext context) {
-    Device().init(context);
-
-    void _onDateChanged(DateTime date) => setState(() => _selectedDate = date);
-
-    return Column(
-      children: [
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            DatePickerCarousel(
-              onDateChange: _onDateChanged,
-              selectedDate: _selectedDate,
-            ),
-          ],
-        ),
-      ],
-    );
+    return Container();
   }
 }
