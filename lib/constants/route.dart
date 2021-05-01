bc618c61a13f9a35d7affa13fc4ef5eb29769a57
nelsonombuya <68895654+neodevelopment@users.noreply.github.com>
2021-05-01 08:56:42 +0300

    Chore: Add Device Query to Routes > Done to avoid potential errors on adding and switching routes using navigator

--- lib/constants/route.dart
+++ lib/constants/route.dart
@@ -6,6 +6,7 @@ import '../presentation/pages/sign_in/sign_in_page.dart';
 import '../presentation/pages/sign_up/sign_up_page.dart';
 import '../presentation/pages/splash/splash_page.dart';
 import '../presentation/pages/welcome/welcome_page.dart';
+import 'device_query.dart';
 
 /// This route management system can be found on:
 /// https://oflutter.com/organized-navigation-named-route-in-flutter/
@@ -24,29 +25,39 @@ const String morePage = '/more';
 Route<dynamic> controller(RouteSettings settings) {
   switch (settings.name) {
     case initPage:
-      return MaterialPageRoute(builder: (context) => Init());
+      return MaterialPageRoute(
+          builder: (context) => DeviceQuery(context, Init()));
     case welcomePage:
-      return MaterialPageRoute(builder: (context) => WelcomePage());
+      return MaterialPageRoute(
+          builder: (context) => DeviceQuery(context, WelcomePage()));
     case splashPage:
-      return MaterialPageRoute(builder: (context) => SplashPage());
+      return MaterialPageRoute(
+          builder: (context) => DeviceQuery(context, SplashPage()));
     case signInPage:
-      return MaterialPageRoute(builder: (context) => SignInPage());
+      return MaterialPageRoute(
+          builder: (context) => DeviceQuery(context, SignInPage()));
     case signUpPage:
-      return MaterialPageRoute(builder: (context) => SignUpPage());
+      return MaterialPageRoute(
+          builder: (context) => DeviceQuery(context, SignUpPage()));
     case homePage:
-      return MaterialPageRoute(builder: (context) => HomePage());
+      return MaterialPageRoute(
+          builder: (context) => DeviceQuery(context, HomePage()));
     case dashboardPage:
       return MaterialPageRoute(
-          builder: (context) => HomePage(subPage: HomeSubPage.Dashboard));
+          builder: (context) =>
+              DeviceQuery(context, HomePage(subPage: HomeSubPage.Dashboard)));
     case eventsPage:
       return MaterialPageRoute(
-          builder: (context) => HomePage(subPage: HomeSubPage.Events));
+          builder: (context) =>
+              DeviceQuery(context, HomePage(subPage: HomeSubPage.Events)));
     case tasksPage:
       return MaterialPageRoute(
-          builder: (context) => HomePage(subPage: HomeSubPage.Tasks));
+          builder: (context) =>
+              DeviceQuery(context, HomePage(subPage: HomeSubPage.Tasks)));
     case morePage:
       return MaterialPageRoute(
-          builder: (context) => HomePage(subPage: HomeSubPage.More));
+          builder: (context) =>
+              DeviceQuery(context, HomePage(subPage: HomeSubPage.More)));
     default:
       throw ('This route name does not exit');
   }
