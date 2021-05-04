89ef2f24eee03cd1f069949c432ebfb31232fe45
nelsonombuya <68895654+neodevelopment@users.noreply.github.com>
2021-05-04 12:11:31 +0300

    Fix: Streams not being subscribed to on launch > Didn't emit streams as they were being added

--- lib/cubit/session/session_cubit.dart
+++ lib/cubit/session/session_cubit.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
+import 'package:classmate/data/repositories/unit_repository.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:equatable/equatable.dart';
 
@@ -10,11 +13,11 @@ import '../../data/repositories/user_repository.dart';
 part 'session_state.dart';
 
 class SessionCubit extends Cubit<SessionState> {
-  late final _userDataStream;
-  late final UserRepository _userRepository;
   List<Stream<DocumentSnapshot>> sessionStreamsList = [];
+
+  late final StreamSubscription _userDataStreamSubscription;
+  late final UserRepository _userRepository;
   final AuthRepository _authRepository = AuthRepository();
-  final CourseRepository _courseRepository = CourseRepository();
 
   SessionCubit() : super(SessionInitial([].cast<Stream<DocumentSnapshot>>())) {
     if (!_authRepository.isUserSignedIn()) {
@@ -23,37 +26,40 @@ class SessionCubit extends Cubit<SessionState> {
     }
 
     _userRepository = UserRepository(_authRepository.getCurrentUser()!);
-    _userDataStream = _userRepository.userDataStream;
-    _userDataStream.listen((snapshot) {
-      if (snapshot != null &&
-          snapshot.registeredUnits != null &&
-          snapshot.currentSession) {
-        snapshot.registeredUnits!.forEach((unitReference) {
-          var currentSession;
-          CourseRepository.course(unitReference)
-              .getCourseDetails()
-              .then((value) {
-            currentSession = value.data() != null
+    Stream _userDataStream = _userRepository.userDataStream.asBroadcastStream();
+
+    // * Gets a list of units that the user has registered for
+    _userDataStreamSubscription = _userDataStream.listen((snapshot) {
+      if (snapshot != null && snapshot.registeredUnits != null) {
+        snapshot.registeredUnits!.forEach((unit) {
+          // * Gets the current session for the registered unit
+          UnitRepository().getUnitDetails(unit).then((value) {
+            var currentSession = (value.data() != null &&
+                    value.data()!['currentSession'] != null)
                 ? value.data()!['currentSession']
                 : 'FEB_MAY_2021';
+
+            // * Gets the stream for the current session in the registered unit
+            var stream = SessionRepository(unit, currentSession)
+                .sessionDataStream
+                .asBroadcastStream();
+
+            // * Adds the session's stream to the list of streams to be listened to
+            if (!sessionStreamsList.contains(stream)) {
+              sessionStreamsList.add(stream);
+            }
+
+            // * Emits the new list of streams
+            emit(SessionStreamListChanged(sessionStreamsList));
           });
-
-          var stream = SessionRepository(unitReference, currentSession)
-              .sessionDataStream;
-
-          if (!sessionStreamsList.contains(stream)) {
-            sessionStreamsList.add(stream);
-          }
         });
-
-        emit(SessionStreamListChanged(sessionStreamsList));
       }
     });
   }
 
   @override
   Future<void> close() {
-    _userDataStream.cancel();
+    _userDataStreamSubscription.cancel();
     return super.close();
   }
 }
