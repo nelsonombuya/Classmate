7060b089821b3e57613e6eb4fb36a7f54cc81f41
Nelson Ombuya <68895654+neodevelopment@users.noreply.github.com>
2021-05-14 07:58:42 +0300

    Chore: Fixing Imports on Sign Up & Sign In Pages > Renamed Material to material

--- lib/presentation/pages/sign_up/sign_up_page.dart
+++ lib/presentation/pages/sign_up/sign_up_page.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/Material.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../bloc/sign_up/sign_up_bloc.dart';
