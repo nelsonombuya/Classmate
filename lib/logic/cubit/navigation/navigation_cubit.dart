64c8f8c3a46be3456e019633c808e8a3230940dd
Nelson Ombuya <68895654+neodevelopment@users.noreply.github.com>
2021-05-28 06:58:37 +0300

    Refactor: Reduced Navigation Cubit Code

--- lib/logic/cubit/navigation/navigation_cubit.dart
+++ lib/logic/cubit/navigation/navigation_cubit.dart
@@ -2,12 +2,17 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 
-part 'navigation_state.dart';
-
 class NavigationCubit extends Cubit<NavigationState> {
   NavigationCubit(this.navigatorKey) : super(NavigationState());
 
   final GlobalKey<NavigatorState> navigatorKey;
 
   void popCurrentPage() => navigatorKey.currentState!.pop();
+}
+
+class NavigationState extends Equatable {
+  const NavigationState();
+
+  @override
+  List<Object> get props => [];
 }
